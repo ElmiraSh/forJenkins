95230170bac076789c087ee66f94b824f7eb8f53
Elmira Shaykhattarova <elmirailgizovna@gmail.com>
2023-12-12 20:16:00 +0300

    add code

--- src/test/java/propertis/tests/SystemPropertiesTests.java
+++ src/test/java/propertis/tests/SystemPropertiesTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class SystemPropertiesTests {
-    @Test
-    @Tag("property")
-    void systemPropertiesTest(){
-       String browser= System.getProperty("browser","mozilla");
-       System.out.println(browser);
-    }
+//    @Test
+//    @Tag("property")
+//    void systemPropertiesTest(){
+//       String browser= System.getProperty("browser","mozilla");
+//       System.out.println(browser);
+//    }
 }
